285be037077fa2b5107c266c8d825ab6c5146a13
shakilahazrumy <104100247@student.swin.edu.au>
2025-06-02 14:18:47 +1000

    Changes

--- week2/2.1_CounterTask/obj/Debug/net8.0/CounterTask.AssemblyInfo.cs
+++ week2/2.1_CounterTask/obj/Debug/net8.0/CounterTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CounterTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf768ca0c61156c07589ae6cfce1d604705c6c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c5fd06905cda6f1a0d791f46f42b2dbff8c2a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CounterTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CounterTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
