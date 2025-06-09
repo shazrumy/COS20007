808d807cd3e922eafa399ddc6a4026f516f49ed4
shazrumy <shakila191103@outlook.com>
2025-06-09 22:18:33 +1000

    Commit

---
+++ week12/CounterClockinC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ClockApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Clock MyClock = new Clock();
+            for (int i = 0; i < 10000; i++)
+            {
+                MyClock.Tick();
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Time after 10,000 ticks: " + MyClock.GetTime());
+            Console.WriteLine("Execution time: " + stopwatch.ElapsedMilliseconds + " ms");
+            
+            MyClock.Restart();
+            Console.WriteLine("Time after restart: " + MyClock.GetTime());
+
+            Process process = Process.GetCurrentProcess();
+            Console.WriteLine("Memory usage: " + (process.WorkingSet64 / 1024 / 1024) + " MB");
+        }
+    }
+}
